db9fdb6c34a24e7472a8097e6969c64f79c0f903
Alexandr Mayorov <maerovwork@gmail.com>
2020-05-10 00:03:57 +0300

    add fake date for response
    Signed-off-by: Alexandr Mayorov <maerovwork@gmail.com>

---
+++ server/fackeData/fakeMovie.js
@@ -0,0 +1,7 @@
+export const movies = [
+    { id: '1', title: 'Pulp Fiction', genre: 'Crime' },
+    { id: '2', title: '1984', genre: 'Sci-Fi' },
+    { id: 3, title: 'V for vendetta', genre: 'Sci-Fi-Triller' },
+    { id: 4, title: 'Snatch', genre: 'Crime-Comedy' },
+];
+
