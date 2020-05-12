66c0f1f0f3094068240e7e4c456b39437ede1695
Alexandr Mayorov <maerovwork@gmail.com>
2020-05-12 14:39:32 +0300

    refactoring server
    Signed-off-by: Alexandr Mayorov <maerovwork@gmail.com>

--- server/app.js
+++ server/app.js
@@ -2,20 +2,21 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const movieSchema = require('./schemas/movie/movieSchema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
-mongoose.connect('mongodb://amayorov:dana0801@ds061196.mlab.com:61196/amayorov',  { useNewUrlParser: true } );
+mongoose.connect('mongodb://amayorov:dana0801@ds061196.mlab.com:61196/amayorov',  { useNewUrlParser: true } )
+        .then(() => console.log(`Connection to db`))
+        .catch(err => console.log(`Connection failed with error: ${err}`));
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema: movieSchema,
     graphiql: true,
 }));
-
-const dbConnection = mongoose.connection;
-dbConnection.on('error', err => console.log(`Connection failed with error: ${err}`));
-dbConnection.once('open', () => console.log(`Connection to db`));
 
 app.listen(PORT, err => {
     err ? console.log(err) : console.log('Server started!');
